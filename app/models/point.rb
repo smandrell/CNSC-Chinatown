4f326e164f0e1ac364218be40d683e1e927c0ac4
Ebrahem Farooqui <ebe.farooqui@berkeley.edu>
2017-10-31 23:09:59 +0000

    [feat] Changing new view for points

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,4 +1,9 @@
 class Point < ActiveRecord::Base
     geocoded_by :address
     after_validation :geocode
+    
+    def self.all_categories
+        %w(Dining Historical Attraction)
+    end
+    
 end
