c09e333c48af006427b878081cc7b92d304bd55f
Ebrahem Farooqui <ebe.farooqui@berkeley.edu>
2017-11-04 00:28:31 +0000

    [fix] Added needed comment

--- app/models/point.rb
+++ app/models/point.rb
@@ -2,6 +2,7 @@ class Point < ActiveRecord::Base
     geocoded_by :address
     after_validation :geocode
     
+    # All possible categories for a point of interest.
     def self.all_categories
         %w('Dining', 'Historical', 'Attraction')
     end
