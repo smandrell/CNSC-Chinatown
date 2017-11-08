8134dcbcac885fe547ede9ba721d7660de62b5c8
Xintong Wen <wenb@berkeley.edu>
2017-11-08 00:17:16 +0000

    [feat] points rendered on map page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,9 @@ class WelcomeController < ApplicationController
   end
   
   def map
-    @festivals = Point.all
+    @points = Point.all
     @hash = Gmaps4rails.build_markers(@points) do |pt, marker|
+      next if pt.latitude.nil? || pt.longitude.nil?
       marker.lat pt.latitude
       marker.lng pt.longitude
       marker.infowindow pt.name
