4a80b96f5293feb31c3ca1a0497540a74cff04fb
Ebrahem Farooqui <ebe.farooqui@berkeley.edu>
2017-11-10 23:53:27 +0000

    All tests passing

--- features/step_definitions/poi_steps.rb
+++ features/step_definitions/poi_steps.rb
@@ -8,11 +8,32 @@ Given /the following points of interest exist/ do |points_table|
     end
 end
 
+Given /^(?:|I )want to "([^"]*)" point "([^"]*)"$/ do |command, point|
+   first(:link, command).click
+end
+
 Then /(.*) seed points should exist/ do |n_seeds|
     Point.count.should be n_seeds.to_i
 end
 
+Then /I press "(.*)" on the popup window/ do |command|
+  begin
+    main, popup = page.driver.browser.window_handles
+    within_window(popup) do
+      click_on(command)
+    end
+  rescue
+  end
+end
 
+    
+Then /^(?:|I )should see the point trait "([^"]*)"$/ do |trait|
+    if page.respond_to? :should
+        page.should have_xpath('//*', :text => trait)
+    else
+        assert page.has_xpath?('//*', :text => trait)
+    end
+end
 
 Then /^(?:|I )should see the form field "([^"]*)"$/ do |field|
     if page.respond_to? :should
@@ -21,4 +42,3 @@ Then /^(?:|I )should see the form field "([^"]*)"$/ do |field|
         assert page.has_no_field?(field)
     end
 end
-
