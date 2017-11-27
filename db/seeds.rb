df6790d5145950bd7ad30e32ffa3c5113732c5b3
Scott Mandrell <sdmandrell@berkeley.edu>
2017-11-27 21:11:00 +0000

    [chore] seeding for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+points = [{:name => "Old St. Mary's Cathedral", :address => '660 California St.', :description => 'First Asian church in North America', :category => 'Historical'},
+          {:name => "Tianhou Temple", :address => '125 Waverly Place', :description => 'Oldest Buddhist temple in the US', :category => 'Historical'},
+          {:name => "Dragon's Gate", :address => 'Bush St. & Grant Ave.', :description => 'Only authentic Chinatown Gate in North America', :category => 'Attraction'},
+          {:name => "Hong Kong Clay Pot Restaurant", :address => '960 Grant Ave.', :description => 'Chinese clay pot lunch & dinner spot', :category => 'Dining'},
+          {:name => "Hunan Home's Restaurant", :address => '622 Jackson St.', :description => 'Family owned Chinese cooking with booths', :category => 'Dining'},
+          {:name => "Good Mong Kok Bakery", :address => '1039 Stockton St.', :description => 'Homemade dim sum and baked goods', :category => 'Dining'}
+    ]
 
-points = [{:name => 'Hong Kong Clay Pot City Restaurant', :address => '960 Grant Ave', :latitude => '-122.406511', :longitude => "37.795884", :description => "Clay pots soaked in water cook & steam many of the dishes at this Chinese lunch & dinner spot.", :category => "Dining"},
-        {:name => 'Hunan Homes Restaurant', :address => '622 Jackson St', :latitude => '-122.405694', :longitude => "37.796413", :description => "Fans of “homestyle” Hunanese cooking trust this “reliable” Chinatown spot for “generous” servings of “freshly made” fare with “fabulous” sauces that range from “not spicy” to “really hot” (if you're “feeling strong”, say “make me sweat”); though it has “dated decor”, service is “a cut above” what you'd expect for such a “cheap” price.", :category => "Dining"}
-  	 ]
-
-points.each do |point|
-  Point.create!(point)
+points.each do |pt|
+  Point.create!(pt)
 end
